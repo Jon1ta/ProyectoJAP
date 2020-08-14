11291fa02983dc4e7d7a82e3ac1da39409439bd7
joon1ta <jona_@hotmail.com>
2020-08-13 21:37:19 -0300

    cambios

--- js/login.js
+++ js/login.js
@@ -1,7 +1,7 @@
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
-document.addEventListener("DOMContentLoaded", function(e){
+
 
 document.getElementById('submit').addEventListener('click', function(e) {
 let usuario = document.getElementById('usuario');
@@ -13,13 +13,15 @@ let validacion = true;
 if(usuario.value === "" || contraseña.value === "") {
 alert("Debe completar el Usuario Y la contraseña");
 validacion = false;
-}  else {
-  window.location = "login.html";
-};
+
+} if(validacion) {
+  location.href = 'login.html';
+}
+
+
 
 
   
 });
 
-});
 
